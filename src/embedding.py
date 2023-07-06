7c72e484bf00b8dd50bb3de323306425875a38d1
Lukas Probst <thelukasprobst@gmail.com>
2023-07-06 14:35:32 +0200

    Implement storing raw text into a CSV file as well as tokenization

--- src/embedding.py
+++ src/embedding.py
@@ -1,38 +1,125 @@
+from matplotlib import pyplot as plt
+
+from config import *
+import pandas as pd
+import tiktoken
+from IPython.display import display
 import openai
 
-openai.api_key = "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"
+### Saving the raw text into a CSV file
 
-response = openai.Embedding.create(
-    input="This is an example",
-    engine="text-similarity-davinci-001")
+texts = []
+for root, dirs, files in os.walk(data_dir):
+    if os.path.basename(root) != 'transcription':
+        continue
+    for file in files:
+        with open(os.path.join(root, file), 'r') as f:
+            text = f.read()
+            texts.append((file, text))
+print(texts)
 
-""" import os
+# Blank empty lines can clutter the text files and make them harder to process.
+# This function removes those lines and tidies up the files.
+def remove_newlines(serie):
+    serie = serie.str.replace('\n', ' ')
+    serie = serie.str.replace('\\n', ' ')
+    serie = serie.str.replace('  ', ' ')
+    serie = serie.str.replace('  ', ' ')
+    return serie
 
-parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-data_dir = os.path.join(parent_dir, 'ADReSSo')
-example_file =  os.path.join(data_dir, 'ADReSSo21-diagnosis-train/diagnosis/train/audio/ad/transcription/adrso024')
 
-from sentence_transformers import SentenceTransformer
-model = SentenceTransformer('all-MiniLM-L6-v2')
+# Create a dataframe from the list of texts
+df = pd.DataFrame(texts, columns=['fname', 'text'])
 
-text = []
-with open(example_file, 'r') as f:
-    text.append(f.read())
-print(text)
+# Set the text column to be the raw text with the newlines removed
+df['text'] = df.fname + ". " + remove_newlines(df.text)
+df.to_csv('../processed/scraped.csv')
+df.head()
 
-#Our sentences we like to encode
-#sentences = ['This framework generates embeddings for each input sentence',
-'Sentences are passed as a list of string.',
-'The quick brown fox jumps over the lazy dog.'
 
-#Sentences are encoded by calling model.encode()
+### Tokenization
 
-embeddings = model.encode(example_file)
+# The API has a limit on the maximum number of input tokens for embeddings.
+# To stay below the limit, the text in the CSV file needs to be broken down into multiple rows.
+# The existing length of each row will be recorded first to identify which rows need to be split.
 
-#Print the embeddings
-for sentence, embedding in zip(text, embeddings):
-    print("Sentence:", sentence)
-    print("Embedding:", embedding)
-    print("")
+# Load the cl100k_base tokenizer which is designed to work with the ada-002 model
+tokenizer = tiktoken.get_encoding("cl100k_base")
 
- """
+df = pd.read_csv('../processed/scraped.csv', index_col=0)
+df.columns = ['title', 'text']
+
+# Tokenize the text and save the number of tokens to a new column
+df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+
+# Visualize the distribution of the number of tokens per row using a histogram
+df.n_tokens.hist()
+
+
+display(df)
+plt.show()
+
+max_tokens = 500
+
+
+# Function to split the text into chunks of a maximum number of tokens
+def split_into_many(text, max_tokens=max_tokens):
+    # Split the text into sentences
+    sentences = text.split('. ')
+
+    # Get the number of tokens for each sentence
+    n_tokens = [len(tokenizer.encode(" " + sentence)) for sentence in sentences]
+
+    chunks = []
+    tokens_so_far = 0
+    chunk = []
+
+    # Loop through the sentences and tokens joined together in a tuple
+    for sentence, token in zip(sentences, n_tokens):
+
+        # If the number of tokens so far plus the number of tokens in the current sentence is greater
+        # than the max number of tokens, then add the chunk to the list of chunks and reset
+        # the chunk and tokens so far
+        if tokens_so_far + token > max_tokens:
+            chunks.append(". ".join(chunk) + ".")
+            chunk = []
+            tokens_so_far = 0
+
+        # If the number of tokens in the current sentence is greater than the max number of
+        # tokens, go to the next sentence
+        if token > max_tokens:
+            continue
+
+        # Otherwise, add the sentence to the chunk and add the number of tokens to the total
+        chunk.append(sentence)
+        tokens_so_far += token + 1
+
+    return chunks
+
+
+shortened = []
+
+# Loop through the dataframe
+for row in df.iterrows():
+
+    # If the text is None, go to the next row
+    if row[1]['text'] is None:
+        continue
+
+    # If the number of tokens is greater than the max number of tokens, split the text into chunks
+    if row[1]['n_tokens'] > max_tokens:
+        shortened += split_into_many(row[1]['text'])
+
+    # Otherwise, add the text to the list of shortened texts
+    else:
+        shortened.append(row[1]['text'])
+
+df = pd.DataFrame(shortened, columns = ['text'])
+df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+df.n_tokens.hist()
+plt.show()
+
+df['embeddings'] = df.text.apply(lambda x: openai.Embedding.create(input=x, engine='text-embedding-ada-002')['data'][0]['embedding'])
+
+df.to_csv('processed/embeddings.csv')
+df.head()
