c0ae113957722567da9ae76058ecddc4ad9e95c9
Lukas Probst <thelukasprobst@gmail.com>
2023-07-06 18:14:24 +0200

    Tidy up embedding.py

--- src/embedding.py
+++ src/embedding.py
@@ -2,39 +2,38 @@ from matplotlib import pyplot as plt
 
 from config import *
 import pandas as pd
-import tiktoken
 from IPython.display import display
 import openai
 
-### Saving the raw text into a CSV file
 
-texts = []
-for root, dirs, files in os.walk(data_dir):
-    if os.path.basename(root) != 'transcription':
-        continue
-    for file in files:
-        with open(os.path.join(root, file), 'r') as f:
-            text = f.read()
-            texts.append((file, text))
-print(texts)
+# Saving the raw text into a CSV file
+def text_to_csv(data_dir):
+    texts = []
+    for root, dirs, files in os.walk(data_dir):
+        if os.path.basename(root) != 'transcription':
+            continue
+        for file in files:
+            with open(os.path.join(root, file), 'r') as f:
+                text = f.read()
+                texts.append((file, text))
+    print(texts)
 
-# Blank empty lines can clutter the text files and make them harder to process.
-# This function removes those lines and tidies up the files.
-def remove_newlines(serie):
-    serie = serie.str.replace('\n', ' ')
-    serie = serie.str.replace('\\n', ' ')
-    serie = serie.str.replace('  ', ' ')
-    serie = serie.str.replace('  ', ' ')
-    return serie
+    # Blank empty lines can clutter the text files and make them harder to process.
+    # This function removes those lines and tidies up the files.
+    def remove_newlines(serie):
+        serie = serie.str.replace('\n', ' ')
+        serie = serie.str.replace('\\n', ' ')
+        serie = serie.str.replace('  ', ' ')
+        serie = serie.str.replace('  ', ' ')
+        return serie
 
+    # Create a dataframe from the list of texts
+    df = pd.DataFrame(texts, columns=['fname', 'text'])
 
-# Create a dataframe from the list of texts
-df = pd.DataFrame(texts, columns=['fname', 'text'])
-
-# Set the text column to be the raw text with the newlines removed
-df['text'] = df.fname + ". " + remove_newlines(df.text)
-df.to_csv('../processed/scraped.csv')
-df.head()
+    # Set the text column to be the raw text with the newlines removed
+    df['text'] = df.fname + ". " + remove_newlines(df.text)
+    df.to_csv('processed/scraped.csv')
+    df.head()
 
 
 ### Tokenization
@@ -42,84 +41,82 @@ df.head()
 # The API has a limit on the maximum number of input tokens for embeddings.
 # To stay below the limit, the text in the CSV file needs to be broken down into multiple rows.
 # The existing length of each row will be recorded first to identify which rows need to be split.
+def tokenization(tokenizer):
+    df = pd.read_csv('processed/scraped.csv', index_col=0)
+    df.columns = ['title', 'text']
 
-# Load the cl100k_base tokenizer which is designed to work with the ada-002 model
-tokenizer = tiktoken.get_encoding("cl100k_base")
+    # Tokenize the text and save the number of tokens to a new column
+    df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
 
-df = pd.read_csv('../processed/scraped.csv', index_col=0)
-df.columns = ['title', 'text']
+    # Visualize the distribution of the number of tokens per row using a histogram
+    df.n_tokens.hist()
 
-# Tokenize the text and save the number of tokens to a new column
-df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+    display(df)
+    plt.show()
 
-# Visualize the distribution of the number of tokens per row using a histogram
-df.n_tokens.hist()
+    max_tokens = 500
 
+    # Function to split the text into chunks of a maximum number of tokens
+    def split_into_many(text, max_tokens=max_tokens):
+        # Split the text into sentences
+        sentences = text.split('. ')
 
-display(df)
-plt.show()
+        # Get the number of tokens for each sentence
+        n_tokens = [len(tokenizer.encode(" " + sentence)) for sentence in sentences]
 
-max_tokens = 500
+        chunks = []
+        tokens_so_far = 0
+        chunk = []
 
+        # Loop through the sentences and tokens joined together in a tuple
+        for sentence, token in zip(sentences, n_tokens):
 
-# Function to split the text into chunks of a maximum number of tokens
-def split_into_many(text, max_tokens=max_tokens):
-    # Split the text into sentences
-    sentences = text.split('. ')
+            # If the number of tokens so far plus the number of tokens in the current sentence is greater
+            # than the max number of tokens, then add the chunk to the list of chunks and reset
+            # the chunk and tokens so far
+            if tokens_so_far + token > max_tokens:
+                chunks.append(". ".join(chunk) + ".")
+                chunk = []
+                tokens_so_far = 0
 
-    # Get the number of tokens for each sentence
-    n_tokens = [len(tokenizer.encode(" " + sentence)) for sentence in sentences]
+            # If the number of tokens in the current sentence is greater than the max number of
+            # tokens, go to the next sentence
+            if token > max_tokens:
+                continue
 
-    chunks = []
-    tokens_so_far = 0
-    chunk = []
+            # Otherwise, add the sentence to the chunk and add the number of tokens to the total
+            chunk.append(sentence)
+            tokens_so_far += token + 1
 
-    # Loop through the sentences and tokens joined together in a tuple
-    for sentence, token in zip(sentences, n_tokens):
+        return chunks
 
-        # If the number of tokens so far plus the number of tokens in the current sentence is greater
-        # than the max number of tokens, then add the chunk to the list of chunks and reset
-        # the chunk and tokens so far
-        if tokens_so_far + token > max_tokens:
-            chunks.append(". ".join(chunk) + ".")
-            chunk = []
-            tokens_so_far = 0
+    shortened = []
 
-        # If the number of tokens in the current sentence is greater than the max number of
-        # tokens, go to the next sentence
-        if token > max_tokens:
+    # Loop through the dataframe
+    for row in df.iterrows():
+
+        # If the text is None, go to the next row
+        if row[1]['text'] is None:
             continue
 
-        # Otherwise, add the sentence to the chunk and add the number of tokens to the total
-        chunk.append(sentence)
-        tokens_so_far += token + 1
+        # If the number of tokens is greater than the max number of tokens, split the text into chunks
+        if row[1]['n_tokens'] > max_tokens:
+            shortened += split_into_many(row[1]['text'])
 
-    return chunks
+        # Otherwise, add the text to the list of shortened texts
+        else:
+            shortened.append(row[1]['text'])
+
+    df = pd.DataFrame(shortened, columns=['text'])
+    df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
+    df.n_tokens.hist()
+    plt.show()
+    return df
 
 
-shortened = []
+def create_embeddings(df):
+    df['embeddings'] = df.text.apply(
+        lambda x: openai.Embedding.create(input=x, engine='text-embedding-ada-002')['data'][0]['embedding'])
 
-# Loop through the dataframe
-for row in df.iterrows():
-
-    # If the text is None, go to the next row
-    if row[1]['text'] is None:
-        continue
-
-    # If the number of tokens is greater than the max number of tokens, split the text into chunks
-    if row[1]['n_tokens'] > max_tokens:
-        shortened += split_into_many(row[1]['text'])
-
-    # Otherwise, add the text to the list of shortened texts
-    else:
-        shortened.append(row[1]['text'])
-
-df = pd.DataFrame(shortened, columns = ['text'])
-df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
-df.n_tokens.hist()
-plt.show()
-
-df['embeddings'] = df.text.apply(lambda x: openai.Embedding.create(input=x, engine='text-embedding-ada-002')['data'][0]['embedding'])
-
-df.to_csv('../processed/embeddings.csv')
-df.head()
+    df.to_csv('processed/embeddings.csv')
+    df.head()
